9a79b5563a6d74ce6208b759bcf02cbfe71ff2ee
vishant solanki <solankivishant01@gmail.com>
2020-07-18 04:43:49 +0530

    file

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "=====Build====="
 echo "=====Build====="
-docker image build -t vishantsolanki01/reactapp .
+docker image build -t reactapp .
+docker image rm $(docker image ls --filter label=owner="raja" -q)
+docker image tag reactapp vishantsolanki01/reactapp:${BUILD_ID}
 
